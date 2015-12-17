01b1fe9686c4deffc9b22a8a31ccd2dc77ab4e37
David Towson <david115632042@gmail.com>
2015-12-16 23:50:01 -0500

    asdf'

--- routes/video.js
+++ routes/video.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('video', { title: 'Express' });
 });
 
 module.exports = router;
