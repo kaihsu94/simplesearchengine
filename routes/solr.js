6999700534e8a06939e4b22f624775912b725545
David Towson <david115632042@gmail.com>
2015-12-17 22:39:08 -0500

    asf

--- routes/solr.js
+++ routes/solr.js
@@ -5,68 +5,66 @@ var solr = require('solr-client');
 var client = solr.createClient();
 /* GET users listing. */
 
-/*
 
-[{"Alabama", "Ala.", "AL"},
-    {"Alaska", "Alaska", "AK"},
-    {"American Samoa", "AS"},
-    {"Arizona",	"Ariz.",	"AZ"},
-    {"Arkansas",	"Ark.",	"AR"},
-    {"California",	"Calif.",	"CA"},
-    {"Colorado",	"Colo.",	"CO"},
-    {"Connecticut",	"Conn.",	"CT"},
-    {"Delaware",	"Del.",	"DE"},
-    {"Dist. of Columbia",	"D.C".,	"DC"},
-{"Florida",	"Fla.",	"FL"},
-{"Georgia",	"Ga.",	"GA"},
-{"Guam",	"Guam",	"GU"},
-{"Hawaii",	"Hawaii",	"HI"},
-{"Idaho",	"Idaho",	"ID"},
-{"Illinois",	"Ill.",	"IL"},
-{"Indiana", "Ind.", "IN"},
-{"Iowa",	"Iowa",	"IA"},
-{"Kansas",	"Kans.",	"KS"},
-{"Kentucky", "Ky.",	"KY"},
-{"Louisiana", "La.",	"LA"},
-{"Maine", "Maine","ME"},
-{"Maryland", "Md.",	"MD"},
-{"Marshall Islands", "MH"},
-{"Massachusetts",	"Mass.",	"MA"},
-{"Michigan",	"Mich.",	"MI"},
-{"Micronesia",	 	"FM"},
-{"Minnesota",	"Minn.",	"MN"},
-{"Mississippi",	"Miss.",	"MS"},
-{"Missouri",	"Mo.",	"MO"},
-{"Montana",	"Mont.",	"MT"},
-{"Nebraska",	"Nebr.",	"NE"},
-{"Nevada",	"Nev.",	"NV"},
-{"New Hampshire",	"N.H.",	"NH"},
-{"New Jersey",	"N.J.",	"NJ"},
-{"New Mexico",	"N.M.",	"NM"},
-{"New York",	"N.Y.",	"NY"},
-{"North Carolina",	"N.C.",	"NC"},
-{"North Dakota",	"N.D.",	"ND"},
-{"Northern Marianas",	 	"MP"},
-{"Ohio",	"Ohio",	"OH"},
-{"Oklahoma",	"Okla.",	"OK"},
-{"Oregon",	"Ore.",	"OR"},
-{"Palau",	 	"PW"},
-{"Pennsylvania",	"Pa.",	"PA"},
-{"Puerto Rico",	"P.R.",	"PR"},
-{"Rhode Island",	"R.I.",	"RI"},
-{"South Carolina",	"S.C.",	"SC"},
-{"South Dakota",	"S.D.",	"SD"},
-{"Tennessee",	"Tenn.",	"TN"},
-{"Texas",	"Tex.",	"TX"},
-{"Utah",	"Utah",	"UT"},
-{"Vermont",	"Vt.",	"VT"},
-{"Virginia",	"Va.",	"VA"}
-{"Virgin Islands",	"V.I.",	"VI"},
-{"Washington",	"Wash.",	"WA"},
-{"West Virginia",	"W.Va.",	"WV"},
-{"Wisconsin",	"Wis.",	"WI"},
-{"Wyoming",	"Wyo.",	"WY"}]
-*/
+var state_list = [["Alabama","Ala.","AL"],
+    ["Alaska","Alaska","AK"],
+    ["American Samoa","AS"],
+    ["Arizona","Ariz.","AZ"],
+    ["Arkansas","Ark.","AR"],
+    ["California","Calif.","CA"],
+    ["Colorado", "Colo.", "CO"],
+    ["Connecticut", "Conn.", "CT"],
+    ["Delaware", "Del.", "DE"],
+    ["Dist. of Columbia", "D.C.", "DC"],
+    ["Florida", "Fla.", "FL"],
+    ["Georgia", "Ga.", "GA"],
+    ["Guam", "Guam", "GU"],
+    ["Hawaii", "Hawaii", "HI"],
+    ["Idaho", "Idaho", "ID"],
+    ["Illinois", "Ill.", "IL"],
+    ["Indiana", "Ind.", "IN"],
+    ["Iowa", "Iowa", "IA"],
+    ["Kansas", "Kans.", "KS"],
+    ["Kentucky", "Ky.", "KY"],
+    ["Louisiana", "La.", "LA"],
+    ["Maine", "Maine","ME"],
+    ["Maryland", "Md.", "MD"],
+    ["Marshall Islands", "MH"],
+    ["Massachusetts", "Mass.", "MA"],
+    ["Michigan", "Mich.", "MI"],
+    ["Micronesia", "FM"],
+    ["Minnesota", "Minn.", "MN"],
+    ["Mississippi", "Miss.", "MS"],
+    ["Missouri", "Mo.", "MO"],
+    ["Montana", "Mont.", "MT"],
+    ["Nebraska", "Nebr.", "NE"],
+    ["Nevada", "Nev.", "NV"],
+    ["New Hampshire", "N.H.", "NH"],
+    ["New Jersey", "N.J.", "NJ"],
+    ["New Mexico", "N.M.", "NM"],
+    ["New York", "N.Y.", "NY"],
+    ["North Carolina", "N.C.", "NC"],
+    ["North Dakota", "N.D.", "ND"],
+    ["Northern Marianas", "MP"],
+    ["Ohio", "Ohio", "OH"],
+    ["Oklahoma", "Okla.", "OK"],
+    ["Oregon", "Ore.", "OR"],
+    ["Palau", "PW"],
+    ["Pennsylvania", "Pa.", "PA"],
+    ["Puerto Rico", "P.R.", "PR"],
+    ["Rhode Island", "R.I.", "RI"],
+    ["South Carolina", "S.C.", "SC"],
+    ["South Dakota", "S.D.", "SD"],
+    ["Tennessee", "Tenn.", "TN"],
+    ["Texas", "Tex.", "TX"],
+    ["Utah", "Utah", "UT"],
+    ["Vermont", "Vt.", "VT"],
+    ["Virginia", "Va.", "VA"],
+    ["Virgin Islands", "V.I.", "VI"],
+    ["Washington", "Wash.", "WA"],
+    ["West Virginia", "W.Va.", "WV"],
+    ["Wisconsin", "Wis.", "WI"],
+    ["Wyoming", "Wyo.", "WY"]];
 
 
 router.get('/', function(req, res, next) {
@@ -93,7 +91,6 @@ router.get('/', function(req, res, next) {
     var base_url = "http://soxkeepyouwarm.davidtowson.com:8983/solr/project_c/select?";
 
 
-
     var is_eng = eng_selected == 'true';
     var is_de = de_selected == 'true';
     var is_es = es_selected == 'true';
@@ -110,12 +107,6 @@ router.get('/', function(req, res, next) {
     } else {
         base_url += "q=text_en%3A" + query + "%0A";
     }
-
-    
-    /*
-    // q=text_en%3ATrump%0A
-    bas_url = "q=text_en%3A" + query + "%0A";
-    base_url += "q=text_en%3A" + query + "%0A";*/
 
     console.log(base_url);
 
@@ -144,9 +135,6 @@ router.get('/', function(req, res, next) {
 
     }
 
-
-
-
     console.log(base_url);
 
     var num_of_results = 10;
@@ -154,9 +142,9 @@ router.get('/', function(req, res, next) {
     var return_url = base_url += "&rows=" + num_of_results;
     return_url += "&wt=json&indent=true";
 
-    res.json({
-        "solr_url":return_url
-    });
+    //res.json({
+    //    "solr_url":return_url
+    //});
 
     base_url += "&rows=10000&wt=json&indent=true";
 
@@ -164,19 +152,32 @@ router.get('/', function(req, res, next) {
     var test_url = "http://soxkeepyouwarm.davidtowson.com:8983/solr/project_c/select?" +
         "q=text_en%3A" + query + "%0A";
 
-    var temp_list = ["ny", "ca"];
+
+    // &fq=location%3A+(*new%5C+york*)      new york
+    // &fq=location%3A+(*New%5C+York*+OR+*N.Y*) New York or N.Y
+
+
     var result_list = [];
 
-    for (var i = 0; i < temp_list.length; i ++) {
-        var current_state = temp_list[i];
+    for (var i = 0; i < state_list.length; i ++) {
+        var current_state_synonyms = state_list[i];
+        var current_state_name = current_state_synonyms[0];
+        current_state_name.replace(' ', '%5C+');
+
+        console.log("new state name is: " + current_state_name);
+
         var local_query = test_url;
-        local_query += "&fq=location%3A+*New%5C+York*";
+        local_query += ("&fq=location%3A+(*" + current_state_name + "*)");
         local_query += "&rows=0&wt=json&indent=true";
+
+        console.log("QUERY IS: " + local_query);
 
         $.ajax({
             method: 'GET',
             url: local_query
         }).then(function successCallback(response) {
+
+            console.log("reached sucessCallback, response is: " + JSON.stringify(response));
 
             var result = {"state":current_state,
                 "count":response.numFound};
@@ -188,7 +189,7 @@ router.get('/', function(req, res, next) {
 
         }, function errorCallback(err) {
 
-            console.log("CALLBACK ERROR FROM HUGE SOLR REQUEST");
+            console.log("CALLBACK ERROR FROM SOLR REQUEST");
             res.json({
                 "solr_url": err
             });
